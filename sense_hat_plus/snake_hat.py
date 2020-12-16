7cf8a259824f5c740aa07392454aa70d70bf1509
Jacob Callear <jacob18@callear.net>
2020-12-16 13:57:41 +0000

    Add food pixels to snake game

--- sense_hat_plus/snake_hat.py
+++ sense_hat_plus/snake_hat.py
@@ -12,30 +12,21 @@ class GameOver(Exception):
 class SnakeHat(SenseHat):
     '''Describes the 8x8 LED grid for a snake game.'''
 
-    opposite_directions = {
-        'left': 'right',
-        'right': 'left',
-        'up': 'down',
-        'down': 'up'
-    }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        random_coord = tuple(randint(0, 7) for _ in range(2))
-        # Describe position of snake on LED grid
-        self.snake = deque([random_coord])
-        self.free_coords = {
-            (x, y)
-            for x in range(8)
-            for y in range(8)
-        }
-        # Light pixels on board
+    def __init__(self):
+        super().__init__()
+        # Light pixels
         self.clear()
-        self.set_pixel(random_coord[0], random_coord[1], (255, 0, 0))
-        self.free_coords.remove(random_coord)
+        snake_pixel = self.set_random_pixel(colour=(255, 0, 0))
+        # Describe position of snake on LED grid
+        self.snake = deque([snake_pixel])
+        self.snake_set = set(self.snake)
+        self.need_food = True
 
     def advance(self, direction):
         '''Add pixel to head of snake in given direction.'''
+        # Add food if needed
+        self.add_food()
+        # Determine new coordinate
         x, y = self.snake[-1]
         if direction == 'up':
             y -= 1
@@ -45,15 +36,35 @@ class SnakeHat(SenseHat):
             x -= 1
         elif direction == 'right':
             x += 1
+        # End game if hit body
+        coord_to_add = (x, y)
+        if coord_to_add in self.snake_set:
+            raise GameOver('Hit yourself')
         # End game if hit edge of board
-        for coord in (x, y):
+        for coord in coord_to_add:
             if coord < 0 or coord > 7:
                 raise GameOver(f'Hit edge of board (x={x}, y={y})')
+        # End game if snake fills all board
+        if len(self.snake) == 64:
+            raise GameOver('You won!')
         # Advance snake head
-        self.snake.append((x, y))
+        self.snake.append(coord_to_add)
         self.set_pixel(x, y, (255, 0, 0))
-        self.free_coords.remove((x, y))
-        # Remove snake tail
-        tail = self.snake.popleft()
-        self.set_pixel(tail[0], tail[1], (0, 0, 0))
-        self.free_coords.add(tail)
+        self.snake_set.add(coord_to_add)
+        # Remove snake tail if not eaten food
+        if coord_to_add != self.food_coord:
+            tail = self.snake.popleft()
+            self.set_pixel(tail[0], tail[1], (0, 0, 0))
+            self.snake_set.remove(tail)
+        else:
+            self.need_food = True
+
+    def add_food(self):
+        if self.need_food:
+            self.food_coord = self.set_random_pixel(colour=(255, 255, 255))
+
+    def set_random_pixel(self, colour=(255, 255, 255)):
+        '''Set random pixel to given colour and return (x, y) coordinates.'''
+        random_pixel = (randint(0, 7), randint(0, 7))
+        self.set_pixel(randint(0, 7), randint(0, 7), colour)
+        return random_pixel
