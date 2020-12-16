f669ec932ed9f33cc3166efada4cf8d938f4d272
Jacob Callear <jacob18@callear.net>
2020-12-16 11:08:37 +0000

    Add snake advance method

--- sense_hat_plus/snake_hat.py
+++ sense_hat_plus/snake_hat.py
@@ -4,9 +4,14 @@ from random import randint
 from sense_hat import SenseHat
 
 
+class GameOver(Exception):
+    '''Raised when snake game is lost.'''
+    pass
+
+
 class SnakeHat(SenseHat):
     '''Describes the 8x8 LED grid for a snake game.'''
-    
+
     opposite_directions = {
         'left': 'right',
         'right': 'left',
@@ -28,3 +33,28 @@ class SnakeHat(SenseHat):
         self.clear()
         self.set_pixel(random_coord[0], random_coord[1], (255, 0, 0))
         self.free_coords.remove(random_coord)
+
+    def advance(self, direction):
+        '''Add pixel to head of snake in given direction.'''
+        x, y = self.snake[-1]
+        if direction == 'up':
+            y -= 1
+        elif direction == 'down':
+            y += 1
+        elif direction == 'left':
+            x -= 1
+        elif direction == 'right':
+            x += 1
+        # End game if hit edge of board
+        if 0 > x > 7:
+            raise GameOver(f'Hit edge of board (x={x})')
+        if 0 > y > 7:
+            raise GameOver(f'Hit edge of board (y={y})')
+        # Advance snake head
+        self.snake.append((x, y))
+        self.set_pixel(x, y, (255, 0, 0))
+        self.free_coords.remove((x, y))
+        # Remove snake tail
+        tail = self.snake.popleft()
+        self.set_pixel(tail[0], tail[1], (0, 0, 0))
+        self.free_coords.add(tail)
